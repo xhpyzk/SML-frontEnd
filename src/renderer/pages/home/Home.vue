7b01dbcd4d01e29fe6d6138bab2450915c1a1844
Brycexxx <2531094755@qq.com>
2019-03-26 22:23:38 +0800

    首页

--- src/renderer/pages/home/Home.vue
+++ src/renderer/pages/home/Home.vue
@@ -8,13 +8,38 @@
         <h2>当前所有工程：</h2>
         <div class="table-style">
           <a-table :columns="columns" :dataSource="data" :customRow="handleClick">
-            <a slot="action" slot-scope="text" href="javascript:;">删除</a>
+            <a slot="action" href="javascript:;">删除</a>
             <p slot="expandedRowRender" slot-scope="record" style="margin: 0">{{record.description}}</p>
           </a-table>
         </div>
       </a-layout-content>
       <a-layout-footer>
-        <button></button>
+        <a-row>
+          <a-col :span="8" :offset="4">
+            <a-button type="dashed" size="large" @click="showModal">新建工程</a-button>
+            <a-modal
+              title="Title"
+              :visible="true"
+              @ok="handleOk"
+              :confirmLoading="confirmLoading"
+              @cancel="handleCancel"
+            >
+              <p>{{ModalText}}</p>
+            </a-modal>
+          </a-col>
+          <a-col :span="8">
+            <a-button type="dashed" size="large" @click="showModal">加入工程</a-button>
+            <a-modal
+              title="Title"
+              :visible="visible"
+              @ok="handleOk"
+              :confirmLoading="confirmLoading"
+              @cancel="handleCancel"
+            >
+              <p>{{ModalText}}</p>
+            </a-modal>
+          </a-col>
+        </a-row>
       </a-layout-footer>
     </a-layout>
   </div>
@@ -25,13 +50,39 @@ const columns = [
   { title: '编号', dataIndex: '编号', key: '编号' },
   { title: '工程项目', dataIndex: '工程项目', key: '工程项目' },
   { title: '项目地点', dataIndex: '项目地点', key: '项目地点' },
-  { title: 'Action', dataIndex: '', key: 'x', scopedSlots: { customRender: 'action' } }
+  {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    scopedSlots: { customRender: 'action' }
+  }
 ]
 
 const data = [
-  { key: 1, 工程项目: 'John Brown', 编号: 1, 项目地点: 'New York No. 1 Lake Park', description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.' },
-  { key: 2, 工程项目: 'Jim Green', 编号: 2, 项目地点: 'London No. 1 Lake Park', description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.' },
-  { key: 3, 工程项目: 'Joe Black', 编号: 3, 项目地点: 'Sidney No. 1 Lake Park', description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.' }
+  {
+    key: 1,
+    工程项目: 'John Brown',
+    编号: 1,
+    项目地点: 'New York No. 1 Lake Park',
+    description:
+      'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.'
+  },
+  {
+    key: 2,
+    工程项目: 'Jim Green',
+    编号: 2,
+    项目地点: 'London No. 1 Lake Park',
+    description:
+      'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.'
+  },
+  {
+    key: 3,
+    工程项目: 'Joe Black',
+    编号: 3,
+    项目地点: 'Sidney No. 1 Lake Park',
+    description:
+      'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.'
+  }
 ]
 
 export default {
@@ -39,7 +90,10 @@ export default {
   data () {
     return {
       data,
-      columns
+      columns,
+      ModalText: 'Content of the modal',
+      visible: false,
+      confirmLoading: false
     }
   },
   methods: {
@@ -47,10 +101,26 @@ export default {
       return {
         on: {
           click: () => {
-            this.$router.push({path: `/project/${index}`})
+            this.$router.push({ path: `/project/${index}` })
           }
         }
       }
+    },
+    showModal () {
+      alert('haha')
+      this.visible = true
+    },
+    handleOk (e) {
+      this.ModalText = 'The modal will be closed after two seconds'
+      this.confirmLoading = true
+      setTimeout(() => {
+        this.visible = false
+        this.confirmLoading = false
+      }, 2000)
+    },
+    handleCancel (e) {
+      console.log('Clicked cancel button')
+      this.visible = false
     }
   }
 }
@@ -62,11 +132,9 @@ export default {
   text-align: center;
 }
 #components-layout-demo-basic .ant-layout-footer {
-  color: #fff;
   height: 120px;
   line-height: 120px;
-  /* background:tomato; */
-
+  background: white;
 }
 #components-layout-demo-basic .ant-layout-header {
   background: #7dbcea;
@@ -81,6 +149,7 @@ export default {
   min-height: 120px;
   line-height: 120px;
   height: 500px;
+  background: white;
 }
 #components-layout-demo-basic > .ant-layout {
   margin-bottom: 48px;
